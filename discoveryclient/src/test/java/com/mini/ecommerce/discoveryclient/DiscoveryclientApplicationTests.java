9982512545a58bcc525e3875b9def74266471277
cloudlein <ayubi54321@gmail.com>
2026-03-24 12:12:58 +0700

    feat: Implement Eureka Discovery Server and Spring Cloud Gateway services, and configure CI/CD pipelines.

---
+++ discoveryclient/src/test/java/com/mini/ecommerce/discoveryclient/DiscoveryclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.ecommerce.discoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
